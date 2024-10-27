5e8b3e0e517aabaecd4b79f0cd8bf910db9819df
Haspikur Rahman Shopnil <hrsshopnil@gmail.com>
2024-10-28 00:03:27 +0600

    Initial Commit

---
+++ Concurrency App/Concurrency_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Concurrency_AppApp.swift
+//  Concurrency App
+//
+//  Created by shopnil hasan on 28/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Concurrency_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
